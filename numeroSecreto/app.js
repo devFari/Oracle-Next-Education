fd3922356d8c97b1ff16c18192978fbacc013276
Matheus Henrique <matheushnfarias@gmail.com>
2025-01-17 19:17:53 -0300

    up20250117casa

--- numeroSecreto/app.js
+++ numeroSecreto/app.js
@@ -1,16 +1,22 @@
 alert('Boas vindas ao jogo do número secreto');
-let numeroSecreto = 5;
+let potenciaDe10 = 100;
+// let numeroMaximo = 10 * potenciaDe10;
+let numeroSecreto = parseInt (Math.random() * potenciaDe10 + 1);
 console.log(numeroSecreto);
 let chute;
 let tentativas = 1;
 
+// alert (`o numero minimo é 1 o o maximo é ${numeroMaximo}`)
+
 //enquanto o chute não for igual ao numeroSecreto
 while (chute != numeroSecreto){
-     chute = prompt ("escolha um numero entre 1 e 10");
+     chute = prompt ("escolha um numero entre 1 e 100");
 
      //se o chute for igual ao numerSecreto
     if (chute == numeroSecreto){
-        alert("Você acertou. O numero secreto era " + numeroSecreto + ", ao todo utilizou " + tentativas + " tentativas");
+        
+        break;
+
     } else{
         if (chute < numeroSecreto){
             alert ("você errou, o munero secreto é maior que o seu chute.");
@@ -24,4 +30,5 @@ while (chute != numeroSecreto){
     }
 }
 
-
+let palavraTentativa = tentativas > 1 ? "tentativas" : "tentativa";
+alert(`Você acertou. O numero secreto era ${numeroSecreto}, ao todo utilizou ${tentativas} ${palavraTentativa}`);
