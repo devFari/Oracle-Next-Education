1f70d2491db86c40f5040e1f7dd4004286a2c829
Matheus Henrique <matheushnfarias@gmail.com>
2025-01-21 16:39:40 -0300

    up01trabalho20250121

--- Logica_de_programacao_explore_funcoes_e_listas/projeto/app.js
+++ Logica_de_programacao_explore_funcoes_e_listas/projeto/app.js
@@ -1,4 +1,6 @@
-let numeroSecreto = gerarNumeroAleatorio();
+// let numeroSecreto = gerarNumeroAleatorio();
+let numeroSecreto = 5;
+let tentativas = 1;
 
 // modificando a tag H1 do HTML utilizando JavaScript
 /* let titulo = document.querySelector('h1');
@@ -10,6 +12,7 @@ paragrafo.innerHTML = "escolha um numero entre 1 e 10"; */
 
 //function padão para reaproveitamento de codigo
 //função com parametro
+// função para exibir texto na tela
 function exibirTextoNaTela (tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
@@ -21,10 +24,35 @@ exibirTextoNaTela("p", "Escolha um numero entre 1 e 10");
 //criando uma função para o botão
 function verificarChute(){
     let chute = document.querySelector('input').value;
-    console.log (chute == numeroSecreto)
+    
+    if (chute == numeroSecreto){
+        let palavraTentativa = tentativas > 1 ? "tentativas" : "tentativa";
+        let mensagemTentativa = `O numero secreto era ${numeroSecreto} você ao todo utilizou ${tentativas} ${palavraTentativa}`
+
+        exibirTextoNaTela('h1', 'Você acertou!');
+        exibirTextoNaTela('p', mensagemTentativa);
+
+    } else{
+        if (chute > numeroSecreto){
+            exibirTextoNaTela('h1', 'Você errou')
+            exibirTextoNaTela('p', 'o numero secreto é menor que o seu chute')
+        } else if (chute < numeroSecreto){
+            exibirTextoNaTela('h1', 'Você errou')
+            exibirTextoNaTela('p', 'o numero secreto é maior que o seu chute')
+        };
+    };
+
+    tentativas ++;
+
+};
+
+while (chute === numeroSecreto){
+    verificarChute()
 };
 
 //função com retorno
 function gerarNumeroAleatorio() {
-    return parseInt(Math.random() *10 + 1);
+    return parseInt(Math.random() * 10 + 1);
 };
+
+console.log(numeroSecreto);
