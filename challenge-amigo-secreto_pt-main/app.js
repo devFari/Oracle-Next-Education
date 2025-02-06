ac1037474adcd6e952e9733cde80db2694a2ec86
Matheus Henrique <matheushnfarias@gmail.com>
2025-02-06 09:24:09 -0300

    up01work20250206

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -1,9 +1,38 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 let listaDeNomes = [];
-let nome = document.getElementById('amigo').value;
+// let nome = document.getElementById('amigo').value;
 
 // Adiciona o nome à lista
-listaDeNomes.push(nome);
+function atualizerArray(){
+    listaDeNomes.push(nome);
+};
 
-//função para escrever nome dos amigos
+// função para exibir texto na tela
+function exibirTexto (id, texto){
+    let campo = document.querySelector(id);
+    campo.innerHTML = texto;
+};
 
+//Condição para quando o campo esteja vazio
+function verificarNome(){
+    let nomeInformado = document.querySelector('input').value;
+
+    if (nomeInformado == ''){
+        exibirTexto('resultado', 'por favor, insira um nome valido');
+        return;
+    } else{
+        //a programação vai aqui dentro para escoler um numero aleatorio
+        exibirTexto('resultado', listaDeNomes[nomeAleatorio])
+    };
+
+};
+
+function sortearNome(){
+    let nomeAleatorio = Math.floor (Math.random() * listaDeNomes.length);
+
+    verificarNome();
+};
+
+/* while (){
+    sortearNome();
+}; */
